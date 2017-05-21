5b7c93b4bedbd2d0e60dc8405bc8caf964e29ff2
lobingera <lobingera@gmail.com>
2017-05-21 12:54:19 +0200

    both

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,21 @@ end
 
 include("test_painting.jl")
 
+        output_file_name = "a.cs"
+        surf = L1ScriptSurface(output_file_name,512,512)
+        hdraw(surf,64,8,4) 
+        finish(surf)
+        destroy(surf)
+
+        @test isfile(output_file_name)
+
+        f = open(output_file_name)
+        str_data = read(f)
+        close(f)
+
+        @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
+        rm(output_file_name)
+
 @testset "Vector Surfaces" begin
 
         output_file_name = "a.cs"
@@ -31,17 +46,6 @@ include("test_painting.jl")
 
         @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
         rm(output_file_name)
-
-# display("script")
-#         io = IOBuffer()
-#         surf = L1ScriptSurface(io,512,512)
-#         hdraw(surf,64,8,4) 
-#         finish(surf)
-        
-#         str = String(take!(io))
-#         str_data = Vector{UInt8}(str)
-        
-#         @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
 
 end
 
