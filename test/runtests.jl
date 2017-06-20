3ee57891cfb16fda978c9a5122ead711ea362097
lobingera <lobingera@gmail.com>
2017-06-20 19:21:13 +0200

    run pkg.test cairo only on 32bit windows,

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,40 +13,41 @@ else
     const Test = BaseTestNext
 end
 
-include("test_painting.jl")
+# include("test_painting.jl")
 
-        output_file_name = "a.cs"
-        surf = L1ScriptSurface(output_file_name,512,512)
-        hdraw(surf,64,8,4) 
-        finish(surf)
-        destroy(surf)
+#         output_file_name = "a.cs"
+#         surf = L1ScriptSurface(output_file_name,512,512)
+#         hdraw(surf,64,8,4) 
+#         finish(surf)
+#         destroy(surf)
 
-        @test isfile(output_file_name)
+#         @test isfile(output_file_name)
 
-        f = open(output_file_name)
-        str_data = read(f)
-        close(f)
+#         f = open(output_file_name)
+#         str_data = read(f)
+#         close(f)
 
-        @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
-        rm(output_file_name)
+#         @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
+#         rm(output_file_name)
 
-@testset "Vector Surfaces" begin
+# @testset "Vector Surfaces" begin
 
-        output_file_name = "a.cs"
-        surf = L1ScriptSurface(output_file_name,512,512)
-        hdraw(surf,64,8,4) 
-        finish(surf)
-        destroy(surf)
+#         output_file_name = "a.cs"
+#         surf = L1ScriptSurface(output_file_name,512,512)
+#         hdraw(surf,64,8,4) 
+#         finish(surf)
+#         destroy(surf)
 
-        @test isfile(output_file_name)
+#         @test isfile(output_file_name)
 
-        f = open(output_file_name)
-        str_data = read(f)
-        close(f)
+#         f = open(output_file_name)
+#         str_data = read(f)
+#         close(f)
 
-        @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
-        rm(output_file_name)
+#         @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
+#         rm(output_file_name)
 
-end
+# end
 
+Pkg.test("Cairo")
 nothing
