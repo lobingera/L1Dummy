9bdc045d36948df4fe3341315176080d99e60a14
lobingera <lobingera@gmail.com>
2018-08-27 20:01:00 +0200

    for travis debug

--- src/L1Dummy.jl
+++ src/L1Dummy.jl
@@ -3,69 +3,19 @@ module L1Dummy
 # package code goes here
 using Graphics
 using Compat
-using Cairo
 
 export L1ScriptSurface
 
-type L1Script <: GraphicsDevice
+mutable struct L1Script <: GraphicsDevice
     ptr::Ptr{Void}
-
-    function L1Script{T<:IO}(stream::T)
-        callback = Cairo.get_stream_callback(T)
-        ptr = ccall((:cairo_script_create_for_stream,Cairo._jl_libcairo), Ptr{Void},
-                (Ptr{Void}, Any), callback, stream)
-        self = new(ptr)
-        finalizer(self, destroy)
-        self
-    end
-
-    function L1Script(filename::AbstractString)
-        ptr = ccall((:cairo_script_create,Cairo._jl_libcairo),
-                    Ptr{Void}, (Ptr{UInt8},), @compat(String(filename)))
-        self = new(ptr)
-        finalizer(self, destroy)
-        self
-    end
 end
 
 function destroy(s::L1Script)
     if s.ptr == C_NULL
         return
     end
-    ccall((:cairo_device_destroy,Cairo._jl_libcairo), Void, (Ptr{Void},), s.ptr)
     s.ptr = C_NULL
     nothing
 end
-
-
-function L1ScriptSurface{T<:IO}(stream::T, w::Real, h::Real)
-    s = L1Script(stream)
-    ptr = ccall((:cairo_script_surface_create,Cairo._jl_libcairo), Ptr{Void},
-                (Ptr{Void},Int32,Float64,Float64),s.ptr ,Cairo.CONTENT_COLOR_ALPHA, w, h)
-    CairoSurface(ptr, w, h)
-end
-
-function L1ScriptSurface(filename::AbstractString, w::Real, h::Real)
-    s = L1Script(filename)
-    ptr = ccall((:cairo_script_surface_create,Cairo._jl_libcairo), Ptr{Void},
-                (Ptr{Void},Int32,Float64,Float64),s.ptr ,Cairo.CONTENT_COLOR_ALPHA, w, h)
-    CairoSurface(ptr, w, h)
-end
-
-    # function L1Script{T<:IO}(stream::T)
-    #     callback = Cairo.get_stream_callback(T)
-    #     ptr = ccall((:cairo_script_create_for_stream,Cairo._jl_libcairo), Ptr{Void},
-    #             (Ptr{Void}, Any), callback, stream)
-    #     self = Cairo.CairoScript(ptr)
-    #     finalizer(self, destroy)
-    #     self
-    # end
-
-    # function CairoScriptSurface{T<:IO}(stream::T, w::Real, h::Real)
-    # s = CairoScript(stream)
-    # ptr = ccall((:cairo_script_surface_create,Cairo._jl_libcairo), Ptr{Void},
-    #             (Ptr{Void},Int32,Float64,Float64),s.ptr ,Cairo.CONTENT_COLOR_ALPHA, w, h)
-    # CairoSurface(ptr, w, h)
-    # end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,3 @@
-using Cairo
 using Compat, Colors
 import Compat.String
 
@@ -13,44 +12,4 @@ else
     const Test = BaseTestNext
 end
 
-# include("test_painting.jl")
-
-#         output_file_name = "a.cs"
-#         surf = L1ScriptSurface(output_file_name,512,512)
-#         hdraw(surf,64,8,4) 
-#         finish(surf)
-#         destroy(surf)
-
-#         @test isfile(output_file_name)
-
-#         f = open(output_file_name)
-#         str_data = read(f)
-#         close(f)
-
-#         @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
-#         rm(output_file_name)
-
-# @testset "Vector Surfaces" begin
-
-#         output_file_name = "a.cs"
-#         surf = L1ScriptSurface(output_file_name,512,512)
-#         hdraw(surf,64,8,4) 
-#         finish(surf)
-#         destroy(surf)
-
-#         @test isfile(output_file_name)
-
-#         f = open(output_file_name)
-#         str_data = read(f)
-#         close(f)
-
-#         @test length(str_data) > 3000 && str_data[1:10] == [0x25,0x21,0x43,0x61,0x69,0x72,0x6f,0x53,0x63,0x72]
-#         rm(output_file_name)
-
-# end
-
-using BinDeps
-BinDeps.debug("Cairo")
-
-Pkg.test("Cairo")
 nothing
